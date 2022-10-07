b2a8ae1346fb6af7580603669d5a3d422d8e08e1
Soheil86 <soheilghanbari1365@gmail.com>
2022-10-07 15:14:42 +0200

    Adding React-Bootstrap & Adding Header Component and Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
 
 function App() {
-  return <h1 className='underline text-3xl'>App</h1>
+  return (
+    <Router>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome To APP</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  )
 }
 
 export default App
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+
+const Header = () => {
+  const logoutHandler = () => {
+    return null
+  }
+
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Banner Creator</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ml-auto'>
+              <NavDropdown title={'TEST'} id='username'>
+                <LinkContainer to='/profile'>
+                  <NavDropdown.Item>Profile</NavDropdown.Item>
+                </LinkContainer>
+                <NavDropdown.Item onClick={logoutHandler}>
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i> Sign In
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  )
+}
+
+export default Header
